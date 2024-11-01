ef4c97681386cfadfc88e14fb14f930433aa0197
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2024-10-31 20:49:30 -0500

    Time: 1 ms (10.85%), Space: 17.4 MB (99.24%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,7 +5,40 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        node, prev = head, None
+        """
+        [1,2,3,4,5]
+        prev = None
+        node = head
+
+        temp = [2,3,4,5]
+        node.next = prev (node = [1, None])
+        prev = node ([1,None])
+        node = temp [2,3,4,5]
+
+        temp = [3,4,5]
+        node.next = [1,None]
+        prev = [2,1, None]
+        node = [3,4,5]
+
+        temp = [4,5]
+        node.next = [2,1,None]
+        prev = [3,2,1,None]
+        node =[4, 5]
+
+        temp = [5]
+        node.next = [3,2,1,None]
+        prev = [4,3,2,1,None]
+        node = [5]
+
+        temp = [None]
+        node.next = [4,2,3,2,1,None]
+        prev = [5,4,3,2,1,None]
+        node = None
+
+        Loop ends when node is None
+
+        """
+        prev, node = None, head
 
         while node:
             temp = node.next
@@ -14,3 +47,4 @@ class Solution:
             node = temp
 
         return prev    
+        
