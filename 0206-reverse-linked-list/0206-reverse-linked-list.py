c890a8722ddffcc67cddeb01d4f46d74b86ee649
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2024-11-06 14:01:22 -0600

    Time: 0 ms (100%), Space: 17.7 MB (42.73%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -6,45 +6,15 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         """
-        [1,2,3,4,5]
-        prev = None
-        node = head
-
-        temp = [2,3,4,5]
-        node.next = prev (node = [1, None])
-        prev = node ([1,None])
-        node = temp [2,3,4,5]
-
-        temp = [3,4,5]
-        node.next = [1,None]
-        prev = [2,1, None]
-        node = [3,4,5]
-
-        temp = [4,5]
-        node.next = [2,1,None]
-        prev = [3,2,1,None]
-        node =[4, 5]
-
-        temp = [5]
-        node.next = [3,2,1,None]
-        prev = [4,3,2,1,None]
-        node = [5]
-
-        temp = [None]
-        node.next = [4,2,3,2,1,None]
-        prev = [5,4,3,2,1,None]
-        node = None
-
-        Loop ends when node is None
-
+        Iterative Approach
         """
-        prev, node = None, head
+        prev, curr = None, head
 
-        while node:
-            temp = node.next
-            node.next = prev
-            prev = node
-            node = temp
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
 
         return prev    
         
