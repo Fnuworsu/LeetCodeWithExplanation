e12d6cd3d314d56c5680647ac7d80b977f85fa0a
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2024-11-17 17:20:31 -0600

    Time: 739 ms (25.04%), Space: 36.8 MB (25.03%) - LeetHub

---
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+        nums.sort()
+        count = defaultdict(int)
+        for i in nums: count[i] +=1 
+            
+        def maxFreq(target):
+            # print(target)
+            left = bisect_left(nums, target-k)
+            right = bisect_left(nums, target+k+1)
+            # print(left, right)
+            options = right-left-count[target]
+            return min(options, numOperations) + count[target]
+
+        ans = 0
+        for i in range(1,max(nums)+1):
+            ans = max(ans, maxFreq(i))
+        return ans
