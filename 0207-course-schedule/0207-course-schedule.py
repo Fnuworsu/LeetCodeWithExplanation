1a44a05000d03db4590aba80e366bc50140fbca4
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2024-12-02 12:25:30 -0600

    Time: 3 ms (88.58%), Space: 18.5 MB (27.26%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,34 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        visited = set()
         graph = defaultdict(list)
+        """
+        0 = not visited, 1 = visited, 2 = done 
+        """
+        state = [0 for _ in range(numCourses)] 
 
         for course, preq in prerequisites:
-            graph[course].append(preq)    
-        
-        def canComplete(course, graph):
-            #cycle detection
-            if course in visited:
+            graph[course].append(preq)
+
+        def completeCourse(course, graph):
+            if state[course] == 1:
                 return False
 
-            if graph[course] == []:
+            if state[course] == 2:
                 return True
 
-            visited.add(course)
+            state[course] = 1
 
             for preq in graph[course]:
-                if not canComplete(preq, graph):
+                if not completeCourse(preq, graph):
                     return False
 
-            #done with that course
-            visited.remove(course)
-            #mark as completed
-            graph[course] = []  
+            state[course] = 2
+            return True   
 
-            return True                  
-        
         for course in range(numCourses):
-            if not canComplete(course, graph):
+            if not completeCourse(course, graph):
                 return False
 
-        return True        
+        return True                         
+                
+        
