8317ee62dd9a97671000f2685794bd4f099dd8b3
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2024-12-15 17:21:32 -0600

    Time: 9 ms (42.63%), Space: 21.2 MB (10.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,18 +1,23 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda x:x[0])
-        res = []
+        """
+        [1,8] [2,6] [8,10] [15,18]
+        overlap = [1,8] [2,6] s=min e= max [1,6]
+        []
+        """
+        intervals.sort()
         row = intervals[0]
+        res = []
 
-        for s, e in intervals[1:]:
+        for s,e in intervals[1:]:
+            #overlap
             if row[1] >= s:
                 row[1] = max(row[1], e)
             else:
                 res.append(row)
-                row = [s, e]
-        #rem
-        res.append(row) 
-
+                row = [s,e]
+        #excess        
+        res.append(row)
         return res            
 
         
