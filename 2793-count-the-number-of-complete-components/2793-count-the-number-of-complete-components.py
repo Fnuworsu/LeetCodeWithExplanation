9016d3ee1ba2993afb855df4a39c88d0b12b79ac
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2024-12-27 03:45:33 -0600

    Time: 83 ms (18.16%), Space: 18.2 MB (18.93%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
@@ -1,43 +1,54 @@
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        rank = [1 for _ in range(n)]
+        pars = [x for x in range(n)]
+
+        def find(a):
+            p = pars[a]
+            while p != pars[p]:
+                p = pars[p]
+            return p
+
+        def union(a,b):
+            pa = find(a)
+            pb = find(b)
+
+            if pa != pb:
+                if rank[pa] > rank[pb]:
+                    rank[pa] += rank[pb]
+                    pars[pb] = pars[pa]
+                else:
+                    rank[pb] += rank[pa]
+                    pars[pa] = pars[pb]
+
         graph = defaultdict(list)
+        components = defaultdict(list)
 
         for a,b in edges:
+            union(a,b)
             graph[a].append(b)
             graph[b].append(a)
 
-        visited = set()
+        for node in range(n):
+            components[find(node)].append(node)
 
-        def dfs(node, components):
-            if node not in visited:
-                components.append(node)
+        def isComplete(comp):
+            nC, eC = len(comp), 0
+            if not comp: return False
+            for node in comp:
+                eC += len(graph[node])
+            eC //= 2
 
-            visited.add(node)
+            return eC == (nC * (nC-1)) // 2               
+    
 
-            for nb in graph[node]:
-                if nb not in visited:
-                    dfs(nb, components)
-
-        def complete(components):
-            nCount, eCount = len(components), 0
-
-            if not components: return False
-
-            for node in components:
-                eCount += len(graph[node])
-            eCount //=2
-            
-            return eCount == (nCount * (nCount - 1)) // 2
+        print(components)    
         res = 0
 
-        for node in range(n):
-            components = []
-            dfs(node, components)
-            print(node, "=>",components)
-
-            if complete(components):
+        for comp in components.values():
+            if isComplete(comp):
                 res += 1
 
-        return res                         
-                                    
+        return res        
+
         
