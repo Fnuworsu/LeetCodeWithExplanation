ca3b450d10b428e3d73d69dad30a1a5047a158f4
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-01-01 23:39:20 -0600

    Time: 689 ms (5.03%), Space: 17.6 MB (15.02%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,38 +1,12 @@
-#using top-down(no memo)
-"""class Solution:
+class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
+        memo = {}
 
-        return self.fib(n-1) + self.fib(n-2) """
-
-#using top-down(memo)
-"""class Solution:
-    def fib(self, n: int) -> int:
         if n == 0: return 0
         if n == 1: return 1
 
-        memo = {}
+        if n in memo: return memo[n]
 
-        if n-1 not in memo:
-            memo[n-1] = self.fib(n-1)
-        if n-2 not in memo:
-            memo[n-2] = self.fib(n-2)
+        memo[n] = self.fib(n-1) + self.fib(n-2)
 
-        return memo[n-1] + memo[n-2]   """      
-
-#using bottom-up(dp)
-class Solution:
-    def fib(self, n: int) -> int:   
-        if n == 0: return 0
-        
-        dp = [0] * (n + 1)
-        dp[0] = 0
-        dp[1] = 1
-
-        for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-
-        return dp[n]        
+        return memo[n]
