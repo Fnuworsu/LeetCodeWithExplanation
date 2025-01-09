de12e786f9bc6abacc2befdf13eb97bd018769d7
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-01-08 19:30:18 -0600

    Time: 11 ms (5.73%), Space: 18.2 MB (9.54%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.py
@@ -1,41 +1,52 @@
-class TrieNode:
-    def __init__(self):
-        self.children = defaultdict(TrieNode)
-        self.end = False
+class KMP:
+    def __init__(self, pattern):
+        self.pattern = pattern
+        self.lps = [0] * len(pattern)
 
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
+        j = 0
+        i = 1
 
-    def add(self, w):
-        curr = self.root 
-        for c in w:
-            curr = curr.children[c]
-        curr.end = True
+        while i < len(pattern):
+            if pattern[i] == pattern[j]:
+                self.lps[i] = j + 1
+                i += 1
+                j += 1
+            elif j == 0:
+                self.lps[i] = 0
+                i += 1
+            else:
+                j = self.lps[j-1]
 
-    def isPrefix(self, w):
-        curr = self.root
-        for c in w:
-            if c not in curr.children:
-                return False
-            curr = curr.children[c]
-        return True                  
+    def match(self, haystack):
+        i = 0
+        j = 0
+
+        while i < len(haystack):
+            if haystack[i] == self.pattern[j]:
+                i += 1
+                j += 1
+            elif j == 0:
+                i += 1
+            else:
+                j = self.lps[j-1]
+            if j == len(self.pattern):
+                return [True, abs(i-j)]
+
+        return [False,-1]                                        
 
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
-        #Using trie
+        #using kmp
         res = 0
 
         for w in words:
-            if w[0] != pref[0]:
+            if len(w) < len(pref):
                 continue
-            if len(pref) > len(w):
+            if w[0] != pref[0]:
                 continue    
-            
-            trie = Trie()
-            trie.add(w)
 
-            if trie.isPrefix(pref):
-                res += 1
+            kmp = KMP(pref)
+            acc = kmp.match(w)
+            if acc[0] and acc[1] == 0: res += 1
 
         return res        
