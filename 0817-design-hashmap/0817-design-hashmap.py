c6421eefd26d9bba1309fbb4f797367991eb00db
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-01-09 02:59:41 -0600

    Time: 56 ms (41.03%), Space: 43.7 MB (7.39%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -0,0 +1,37 @@
+class ListNode:
+    def __init__(self, val, key):
+        self.val = val
+        self.key = key
+
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 10**6 + 7
+        self.acc = [0] * self.size
+    
+    def idx(self, key):
+        return key % self.size
+
+    def put(self, key: int, value: int) -> None:
+        i = self.idx(key)
+        node = ListNode(value, key)
+        self.acc[i] = node
+
+    def get(self, key: int) -> int:
+        i = self.idx(key)
+
+        if self.acc[i] == 0:
+            return -1
+        return self.acc[i].val     
+
+    def remove(self, key: int) -> None:
+        i = self.idx(key)
+        self.acc[i] = 0
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
