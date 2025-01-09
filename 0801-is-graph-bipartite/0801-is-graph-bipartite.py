22b7edaa608455936f0ae3d09fb1df46e268c755
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-01-08 18:01:42 -0600

    Time: 4 ms (25.41%), Space: 18.5 MB (13.65%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,36 +1,28 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        """
-        take node color node blue, children red
-        if color of node and children are the same, return false
-        bfs
-        """
         color = [0 for _ in range(len(graph))]
-
-        def bfs(node):
+        
+        def bipart(node):
             if color[node]:
                 return True
 
             q = deque([node])
-            color[node] = -1 #blue
+            color[node] = 1
 
             while q:
-                node = q.popleft()
-
-                for nb in graph[node]:
-                    if color[node] == color[nb]:
+                curr = q.popleft()
+                for nb in graph[curr]:
+                    if color[curr] == color[nb]:
                         return False
                     elif not color[nb]:
                         q.append(nb)
-                        color[nb] = -1 * color[node]
+                        color[nb] = -1 * color[curr]
 
             return True
 
         for node in range(len(graph)):
-            if not bfs(node):
-                return False  
+            if not bipart(node):
+                return False
 
-        return True                    
-
-
-    
+        return True                           
+        
