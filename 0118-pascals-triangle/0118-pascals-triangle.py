2bca7c20942cca305b96723b1b6a73f50d791c70
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-01-11 12:33:09 -0600

    Time: 0 ms (100%), Space: 17.9 MB (6.49%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -11,7 +11,7 @@ class Solution:
         for i in range(2, numRows):
             res = [1 for _ in range(i+1)]
             for j in range(1,len(res)-1):
-                res[j] = dp[i-1][j-1] + dp[i-1][~j]
+                res[j] = dp[i-1][j-1] + dp[i-1][-j-1]
             dp[i] = res
 
         return dp    
