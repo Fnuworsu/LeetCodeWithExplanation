4faff14d9c76748a513e6caf9d7fb1542ac62226
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-01-13 16:52:01 -0600

    Time: 343 ms (16.81%), Space: 24 MB (11.56%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.py
+++ 1397-search-suggestions-system/1397-search-suggestions-system.py
@@ -1,60 +1,55 @@
-class KMP:
-    def __init__(self, pattern):
-        self.pattern = pattern
-        self.lps = [0] * len(pattern)
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.end = False
 
-        j = 0
-        i = 1
-
-        while i < len(pattern):
-            if pattern[i] == pattern[j]:
-                self.lps[i] = j + 1
-                i += 1
-                j += 1
-            elif j == 0:
-                self.lps[i] = 0
-                i += 1
-            else:
-                j = self.lps[j-1]
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
     
-    def match(self, haystack):
-        i = 0
-        j = 0
-
-        while i < len(haystack):
-            if haystack[i] == self.pattern[j]:
-                i += 1
-                j += 1
-            elif j == 0:
-                i += 1
-            else:
-                j = self.lps[j-1]
-            if j == len(self.pattern):
-                return (i-j) == 0
+    def add(self, word):
+        curr = self.root
         
-        return False
+        for c in word:
+            curr = curr.children[c]
+        
+        curr.end = True
+    
+    def search(self, prefix):
+        def dfs(node, path, res):
+            if len(res) == 3:
+                return
+            if node.end:
+                res.append("".join(path[:]))
+            
+            for k,v in node.children.items():
+                path.append(k)
+                dfs(v, path, res)
+                path.pop()
+        
+        node = self.root
+
+        for c in prefix:
+            node = node.children[c]
+
+        res = []
+        dfs(node, list(prefix), res)
+
+        return res
 
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        res = []
-        pattern = ""
+        trie = Trie()
         products.sort()
 
+        for p in products:
+            trie.add(p)
+        
+        prefix = ""
+        res = []
+
         for c in searchWord:
-            pattern += c
-            acc = []
-            kmp = KMP(pattern)
-
-            for p in products:
-                if kmp.match(p):
-                    acc.append(p)
-
-            if len(acc) > 3:
-                res.append(acc[:3])
-            else:
-                res.append(acc)
+            prefix += c
+            res.append(trie.search(prefix))
         
         return res
-            
-        
-        
