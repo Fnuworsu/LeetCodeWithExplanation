757d26d66c1c5a1d8610e8fb1f2e47b73b0cf755
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-01-16 17:17:05 -0600

    Time: 0 ms (100%), Space: 17.8 MB (27.87%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        @cache
         def dp(i):
             if i == 0:
                 return nums[0]
             if i == 1:
                 return max(nums[0], nums[1])
             
-            return max(dp(i-1), dp(i-2) + nums[i])
+            if i in memo:
+                return memo[i]
+
+            memo[i] = max(dp(i-1), dp(i-2) + nums[i])
+            
+            return memo[i]
         
+        memo = {}
+
         return dp(len(nums)-1)
+        
