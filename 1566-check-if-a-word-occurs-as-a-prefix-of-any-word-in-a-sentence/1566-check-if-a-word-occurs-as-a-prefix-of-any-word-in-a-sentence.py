03888a6266c95483aff05a8689490a23e80518a7
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-01-17 22:30:31 -0600

    Time: 0 ms (100%), Space: 17.9 MB (5.07%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -1,21 +1,51 @@
+class KMP:
+    def __init__(self, pattern):
+        self.pattern = pattern
+        self.lps = [0] * len(pattern)
+
+        j = 0
+        i = 1
+
+        while i < len(pattern):
+            if pattern[i] == pattern[j]:
+                self.lps[i] = j + 1
+                i += 1
+                j += 1
+            elif j == 0:
+                self.lps[i] = 0
+                i += 1
+            else:
+                j = self.lps[j-1]
+    
+    def match(self, haystack):
+        i = 0
+        j = 0
+
+        while i < len(haystack):
+            if haystack[i] == self.pattern[j]:
+                i += 1
+                j += 1
+            elif j == 0:
+                i += 1
+            else:
+                j = self.lps[j-1]
+            if j == len(self.pattern):
+                return not (i-j)
+        
+        return False
+
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
-        #brute force
-        res = []
+        #Using string matching
+        kmp = KMP(searchWord)
+
+        res = float('inf')
         i = 1
 
         for word in sentence.split():
-            prefix = ""
-
-            for c in word:
-                prefix += c
-                if prefix == searchWord:
-                    res.append(i)
-                    break
-            
+            if kmp.match(word):
+                res = min(res, i)
             i += 1
-        
-        res.sort()
 
-        return res[0] if res else - 1
+        return res if res != float('inf') else - 1
         
