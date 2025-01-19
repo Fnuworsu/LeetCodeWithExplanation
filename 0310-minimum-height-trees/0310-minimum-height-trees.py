d3e0c4a6d116245dc64158ece40969aecba135a7
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-01-18 19:18:12 -0600

    Time: 63 ms (43.58%), Space: 30.5 MB (16.32%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,29 +1,36 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        """
+        1.MHT has 1 center(for even nodes) or 2 centers(for odd nodes)
+        2.Condition is while nodes with one degree are > 2
+        3.remove one degree nodes from nb, and delete from the graph afterwards
+        """
         if n == 1:
             return [0]
-
+            
         graph = defaultdict(set)
-        for a,b in edges:
-            graph[a].add(b)
-            graph[b].add(a)
 
-        leaves = deque([node for node in range(n) if len(graph[node]) == 1])
+        for u,v in edges:
+            graph[u].add(v)
+            graph[v].add(u)
+        
+        leaves = deque([node for node in range(n+1) if len(graph[node]) == 1])
         numNodes = n
 
         while numNodes > 2:
-            numLeaves = len(leaves)
-            numNodes -= numLeaves
-
-            for _ in range(numLeaves):
+            leafCount = len(leaves)
+            
+            for _ in range(leafCount):
                 leafNode = leaves.popleft()
-
+                
                 for nb in graph[leafNode]:
                     graph[nb].remove(leafNode)
                     if len(graph[nb]) == 1:
                         leaves.append(nb)
-
-                graph[leafNode] = [] 
-
-        return list(leaves)                       
+                
+                del graph[leafNode]
+            
+            numNodes -= leafCount
+        
+        return list(leaves)
         
