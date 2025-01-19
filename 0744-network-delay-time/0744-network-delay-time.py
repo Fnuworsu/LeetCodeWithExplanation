aa3b42ccb49f70b97df6b4bcea3cd8893ec6da53
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-01-18 19:41:16 -0600

    Time: 367 ms (89.29%), Space: 19.7 MB (48.52%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -1,30 +1,35 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        """
+        1.shortest path to all nodes
+        2.earliest time is max shortest(reaches all nodes)     
+        3. start node = k   
+        """
         graph = defaultdict(list)
 
         for u,v,w in times:
             graph[u].append((v,w))
-
+        
         dist = {node:float('inf') for node in range(1,n+1)}
         dist[k] = 0
-        pq = [(0,k)]
+
+        pq = [(0,k)] #d,node
 
         while pq:
             d, node = heapq.heappop(pq)
 
             if d > dist[node]:
                 continue
-
+            
             for nb,w in graph[node]:
                 newDist = w + d
                 if newDist < dist[nb]:
-                    dist[nb] = newDist  
-                    heapq.heappush(pq, (newDist,nb))
-        # print(dist.values())
-        res = -1
-        for d in dist.values():
-            res = max(res, d)
-
-        return res if res != float('inf') else -1       
-
+                    dist[nb] = newDist
+                    heapq.heappush(pq, (newDist, nb))
         
+        res = -1
+        print(dist.values())
+        for v in dist.values():
+            res = max(res, v)
+        
+        return res if res != float('inf') else -1
