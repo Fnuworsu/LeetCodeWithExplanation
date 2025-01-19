abf2ab74aad0afdfd277eb6b5f75e88547433819
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-01-18 18:57:16 -0600

    Time: 68 ms (6.45%), Space: 62.6 MB (5.85%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.py
@@ -1,26 +1,20 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        n = len(edges) + 1
-        graph = defaultdict(set)
-        
+        graph = defaultdict(list)
+        numSet = set()
+
         for u,v in edges:
-            graph[u].add(v)
-            graph[v].add(u)
+            numSet.add(u)
+            numSet.add(v)
 
-        leaves = deque([node for node in range(1, n+1) if len(graph[node]) == 1])
-        numNodes = n
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        numNodes = len(numSet)-1
 
-        while numNodes > 2:
-            numLeaves = len(leaves)
-            numNodes -= numLeaves
+        for k,v in graph.items():
+            if len(v) == numNodes:
+                return k
 
-            for _ in range(numLeaves):
-                leafNode = leaves.popleft()
-                for nb in graph[leafNode]:
-                    graph[nb].remove(leafNode)
-                    if len(graph[nb]) == 1:
-                        leaves.append(nb)
-
-        return leaves[0]                
 
         
