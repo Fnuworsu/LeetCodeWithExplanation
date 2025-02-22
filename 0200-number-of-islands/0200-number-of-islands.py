4049e4c7e9a1971714748b37a3f04f9a72034d63
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-02-22 17:53:39 -0600

    Time: 266 ms (38.14%), Space: 20.3 MB (58.97%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,35 +1,37 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        # visited = set()
+        dirs = [(1,0), (0,1), (-1,0), (0,-1)]
+
+        def outBounds(r,c):
+            return r < 0 or r > rows-1 or c < 0 or c > cols-1 or grid[r][c] == '0'
 
         def bfs(r,c):
-            q = deque()
-            q.append((r,c))
-            grid[r][c] = '0'
+            q = deque([(r,c)]) 
 
             while q:
-                row, col = q.popleft()
-                dirs = [(1,0), (0,1), (-1,0), (0,-1)]
+                for _ in range(len(q)):
+                    r,c = q.popleft()
+                    grid[r][c] = '0'
 
-                for dr,dc in dirs:
-                    nr = row + dr
-                    nc = col + dc
+                    for dr,dc in dirs:
+                        nr, nc = r + dr, c + dc
 
-                    if nr < 0 or nr > rows-1 or nc < 0 or nc > cols-1 or grid[nr][nc] == '0':
-                        continue
-
-                    q.append((nr,nc))
-                    grid[nr][nc] = '0'
-
-        res = 0
+                        if outBounds(nr,nc):
+                            continue
+                        
+                        q.append((nr,nc))
+                        grid[nr][nc] = '0'
+        
+        count = 0
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1':
-                    res += 1
+                    count += 1
                     bfs(r,c)
+        
+        return count
 
-        return res                            
 
-
+            
