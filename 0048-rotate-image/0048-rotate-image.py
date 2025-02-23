9402b2267b10bc439e0c3e284c2a765289164a58
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-02-22 21:52:10 -0600

    Time: 0 ms (100%), Space: 17.9 MB (22.87%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,43 +3,17 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        #reverse the matrix
-        """
-        1 2 3
-        -----
-        7 8 9
-        gives
+        l, r = 0, len(matrix)-1
 
-        7 8 9
-        4 5 6
-        1 2 3
-
-        transpose
-
-        * 8 9
-        4 * 6
-        1 2 *
-
-        gives
-        7 4 1
-        8 5 2
-        9 6 3
-        """
-        l, r = 0, len(matrix) - 1
-        #reverse the matrix row by row
         while l < r:
             matrix[l], matrix[r] = matrix[r], matrix[l]
 
             l += 1
             r -= 1
-        # print("After rotation")    
-        # print(matrix)
-        # print()
-        #method to find the transpose of a matrix
-        for i in range(len(matrix)):
-            # print(i)
-            # print()
+        
+        n = len(matrix)
+
+        for i in range(n):
             for j in range(i):
-                matrix[i][j], matrix[j][i] =  matrix[j][i], matrix[i][j]
-                # print(matrix)  
-                # print() 
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
