c2df157d22c5bf6e5c27fe7d1f97c157c5f9aef9
Felix Nuworsu <130885230+Fnuworsu@users.noreply.github.com>
2025-04-25 19:14:52 -0500

    Time: 212 ms (7.42%), Space: 41.7 MB (9.78%) - LeetHub

---
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        """
+        total = 10
+        3:4
+        5:3
+        2:2
+        7:1
+        """
+        total = len(arr)
+        K = total // 2
+        nums = sorted([(-v,k) for k,v in Counter(arr).items()])
+        print(nums, K, total)
+        count = 0
+
+        for v,k in nums:
+            total += v
+            count += 1
+
+            if total <= K:
+                return count
+        
+        return count
